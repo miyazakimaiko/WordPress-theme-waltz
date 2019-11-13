8ea46a017bb31b881e7822beff5819591fe4f467
miyazakimaiko <myzkmik19922@gmail.com>
2019-11-13 17:33:16 +0000

    Added label to an input

--- searchform.php
+++ searchform.php
@@ -1,6 +1,9 @@
 <div class="sidebar__contents search-bar">
+    <label for="s" class="sidebar-title">
+        <h3><span>検索フォーム</span></h3>
+    </label>
     <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
-        <input type="search" id="s" name="s" value="" placeholder="キーワードで検索" />
+        <input type="search" id="s" name="s" placeholder="キーワードで検索" />
         <button type="submit" id="searchsubmit" ><?php _e('Search','bonestheme'); ?></button>
     </form>
 </div>
